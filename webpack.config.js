7be57e7d98dcb0009b0a7f7f818b9ddf4816b38d
Jamie Clignett <jamieclignett@Jamies-Mac-mini.local>
2022-08-13 13:23:40 -0700

    next steps

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+// make index.html that talks to appropriate script
+// where does my bundle go - public folder? 
+// webpack dev server on the right port 
+// setting up a proxy
+// adjusting script so that the node server is setup
+// handling file & folder management
+
 module.exports = {
   mode: 'development',
-  entry: './src/server.js',
+  entry: './public/index.js',
   output: {
-    path: path.join(__dirname, '/public'),
+    path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './public/index.html'
-    // template: '!!prerender-loader?string!index.html',
-  })],
+  // plugins: [new HtmlWebpackPlugin({
+  //   template: './public/index.html'
+  //   // template: '!!prerender-loader?string!index.html',
+  // })],
   // target: 'node',
   // devServer: {
   //   port: '3000',
@@ -20,9 +27,9 @@ module.exports = {
   //   hot: true,
   //   liveReload: true,
   // },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts'],
-  },
+  // resolve: {
+  //   extensions: ['.js', '.jsx', '.json', '.ts'],
+  // },
   // stats: {
   //   children: true
   // },
